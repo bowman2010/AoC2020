8575ae753aafa319154802be7419b830ff74c8d4
bowman <bowman@phobos>
2020-12-19 18:44:00 +0100

    Seems clean now, lets continue

--- src/advent2020/Day7/Day7.java
+++ src/advent2020/Day7/Day7.java
@@ -1,19 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package advent2020.Day7;
 
 import advent.AdventClass;
+import java.util.HashMap;
 
 /**
  *
  * @author bowman
  */
 public class Day7 extends AdventClass {
+    HashMap<String, Luggage> luggages; 
     
+    public Day7() {
+        super("Day 7 - Handy Haversacks","");
+        luggages = new HashMap<>();
+    }
+
     
+    public void addLuggage(String definition) {
+        Luggage lug = new Luggage(definition);
+        luggages.put(lug.getColor(), lug);
+    }
     
+    /**
+     * Main execution method
+     */
+    @Override
+    public void run() {
+    }
     
+    /**
+     *  main method for testing 
+     * @param args - not used
+     */
+    public static void main(String[] args) {
+        Day7 d7 = new Day7();
+        d7.run();
+    }
 }
